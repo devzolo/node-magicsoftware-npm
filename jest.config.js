be63e6f06b9345812f1f3411652aba576fe5be25
Raphael-ZoLo <zolotorevsky@gmail.com>
2020-02-05 11:54:30 -0300

    Updates

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'node',
+  verbose: true,
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
+  moduleFileExtensions: ['js', 'ts', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsConfig: './tsconfig.json'
+    },
+    NODE_ENV: 'test'
+  }
+};
---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+describe('Basic Teste', () => {
+  it("0 = 0", () => {
+    expect(0).toBe(0);
+  })
+})
+
+
